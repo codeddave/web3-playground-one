050d5d055423e29436cb14258c1af2a8de425a66
codeddave <drizzzydave@gmail.com>
2021-11-13 14:44:26 +0100

    updated deployment script, used one of the prefunded accts on ganache to send ether to generated address

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,5 +1,10 @@
 const MyContract = artifacts.require("MyContract");
 
-module.exports = function (deployer) {
-  deployer.deploy(MyContract);
+module.exports = async function (deployer, _, accounts) {
+  await deployer.deploy(MyContract);
+  await web3.eth.sendTransaction({
+    from: accounts[0],
+    to: "0x2743981379FfDeD14C2C86d552B31DEBa12d5273",
+    value: web3.utils.toWei("1", "ether"),
+  });
 };
